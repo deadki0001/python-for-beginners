3abcc56f8051c8955e92726579cd61dc54062f59
Devon Adkins <adkins.devon@hotmail.com>
2025-01-21 21:31:46 +0200

    day9

---
+++ day9/dict.py
@@ -0,0 +1,9 @@
+programming_dictionary = {
+"bug": "An error in a program.",
+"Function": "A piece of code that you can esily call again and again.",
+}
+
+
+
+programming_dictionary["Loop"] = " The Action of doing something again and again" 
+print(programming_dictionary)
