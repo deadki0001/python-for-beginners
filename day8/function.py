7ad5d49484fd24ff9015d4a9686538197733e2c1
Devon Adkins <adkins.devon@hotmail.com>
2025-01-21 20:02:00 +0200

    day6 - 8

---
+++ day8/function.py
@@ -0,0 +1,6 @@
+def greet(name, location, age):
+    print(f"I am so tired {name}")
+    print(f"Consistency is really difficult in {location}")
+    print(f"I hope I can make it through this at {age}")
+
+greet(location="Durban", name="Devon", age="35")    
