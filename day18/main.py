2b84f50ef8c0f259cb27055013132620b0796ab6
Devon Adkins <adkins.devon@hotmail.com>
2025-02-15 17:49:20 +0200

    more practice

--- day18/main.py
+++ day18/main.py
@@ -4,21 +4,24 @@ import random
 tim = t()
 
 color = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+directions = [0, 90, 180, 270]
+tim.pensize(width=15)
+tim.speed(speed="fastest")
 
-def draw_shape (num_sides):
-        angle = 360 / num_sides
-        for _ in range(num_sides):
-            tim.forward(100)
-            tim.right(angle)
-
-
-for shape_side_n in range(3, 11):
+for _ in range(1000):
     tim.color(random.choice(color))
-    draw_shape(shape_side_n)  
-
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
 
 
 
 
 screen = Screen()
 screen.exitonclick()
+
+# Tuples are ordered
+# my_tuple = (1, 3, 8)
+# tuples are immutable - they cannot be changed like lists
+
+# Creating a list that stays constant - and you dont want anyone to change the inputs.
+# list(my_tuple) can be converted as simple as this
