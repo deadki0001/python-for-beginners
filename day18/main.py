6578bd0e495c928f8ce58f1869439f4c6815af09
Devon Adkins <adkins.devon@hotmail.com>
2025-02-15 17:29:51 +0200

    more practice

--- day18/main.py
+++ day18/main.py
@@ -1,18 +1,20 @@
 from turtle import Turtle as t, Screen
+import random
 
 tim = t()
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+color = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+def draw_shape (num_sides):
+        angle = 360 / num_sides
+        for _ in range(num_sides):
+            tim.forward(100)
+            tim.right(angle)
 
 
-
-
-
-
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(color))
+    draw_shape(shape_side_n)  
 
 
 
